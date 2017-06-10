6850e04db5d193dec7b8bdfd418b65e1c8584546
Ben Foxall <benfoxall@gmail.com>
2017-06-10 17:54:37 +0100

    All the other stuff

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
+const http = require('http')
+const WebSocket = require('ws')
+const bodyParser = require('body-parser')
+
 const app = express()
+const server = http.createServer(app)
+const wss = new WebSocket.Server({ server })
 
-app.get('/', (req, res) => res.send('hello'))
+app.use(express.static('public'))
 
-app.listen(process.env.PORT || 5000)
+app.post('/', bodyParser.urlencoded({ extended: false }), (req, res) => {
+
+  res.send('two hundred')
+
+  const x = parseFloat(req.body.x)
+  const y = parseFloat(req.body.y)
+  const z = parseFloat(req.body.z)
+  const c = String(req.body.c).slice(0, 20) // lightgoldenrodyellow
+
+  const payload = JSON.stringify({x,y,z,c})
+
+  // broadcast
+  wss
+    .clients
+    .forEach(c => {
+      if(c.readyState === WebSocket.OPEN) c.send(payload)
+    })
+
+})
+
+server.listen(process.env.PORT || 5000, () =>
+  console.log('Listening on %d', server.address().port)
+)
